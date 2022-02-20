2a41a3c52d71cb1e99663edaebdb031be46dc683
daman9999 <damanpreetsingh9699@gmail.com>
2022-02-20 20:05:02 +0530

    punjabi problem solved

---
+++ script.js
@@ -0,0 +1,67 @@
+// actual  conversion code starts here
+
+var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+function convert_millions(num) {
+  if (num >= 1000000) {
+    return convert_millions(Math.floor(num / 1000000)) + " million " + convert_thousands(num % 1000000);
+  } else {
+    return convert_thousands(num);
+  }
+}
+
+function convert_thousands(num) {
+  if (num >= 1000) {
+    return convert_hundreds(Math.floor(num / 1000)) + " thousand " + convert_hundreds(num % 1000);
+  } else {
+    return convert_hundreds(num);
+  }
+}
+
+function convert_hundreds(num) {
+  if (num > 99) {
+    return ones[Math.floor(num / 100)] + " hundred " + convert_tens(num % 100);
+  } else {
+    return convert_tens(num);
+  }
+}
+
+function convert_tens(num) {
+  if (num < 10) return ones[num];
+  else if (num >= 10 && num < 20) return teens[num - 10];
+  else {
+    return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+  }
+}
+
+function convert(num) {
+  if (num == 0) return "zero";
+  else return convert_millions(num);
+}
+
+//end of conversion code
+
+//testing code begins here
+
+function main(val) {
+  var cases = val;
+//   returning the inWords 
+return convert(cases);
+}
+
+
+
+// creating element event in script instead of onClick on script loading 
+// function run huna kuki apa button nu id diti aa 'clickme'
+document.getElementById("clickme").addEventListener("click", function() {
+    // take the value from the text box and put it in val
+    let val = document.getElementById('txt').value;
+    // pass the value to main() and get the in words 8=> eight and store it in variable
+    var inWordsVal=main(val)
+    // get the id of paragraph and put in words in it
+    document.getElementById("p").innerHTML = inWordsVal;
+    // get the id p=of another paragraph and put the number value i.e '8' in it
+    document.getElementById("p1").innerHTML = val;
+  });
